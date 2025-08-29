11e92dbd9ac06ec45503a029c56738e38f2087b1
Enzo Volkmann <enzo@volkmann.dev>
2025-08-29 10:24:49 +0200

    fix: support kirby 5

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 use Composer\Semver\Semver;
 use Kirby\Cms\App as Kirby;
 
-if (Semver::satisfies(Kirby::version() ?? '0.0.0', '~4.0') === false) {
-    throw new Exception('volkmann-design-code/kirby-layout-attributes requires Kirby 4');
+if (Semver::satisfies(Kirby::version() ?? '0.0.0', '^4.0 || ^5.0') === false) {
+    throw new Exception('volkmann-design-code/kirby-layout-attributes requires Kirby 4 or 5');
 }
 
 Kirby::plugin('volkmann-design-code/kirby-layout-attributes', [
--- src/index.js
+++ src/index.js
@@ -41,32 +41,60 @@ window[pluginNamespace].functions.addLayoutAttrs = (layoutField) => {
 
 window.panel.plugin("volkmann-design-code/kirby-layout-attributes", {
   created(Vue) {
-    Vue.$store.subscribeAction({
-      after: (action, state) => {
-        if (action.type === "content/create") {
-          // TODO: Refactor to a more elegant solution to wait for the dom to be ready, e.g. MutationObserver on the block IDs
-          setTimeout(() => {
-            for (const fieldName in action.payload.content) {
-              const field = action.payload.content[fieldName];
-              if (!window[pluginNamespace].functions.isLayoutField(field)) {
-                continue;
-              }
+    const isKirby4 = '$store' in Vue;
+    const isKirby5 = 'events' in window.panel;
+    
+    if (isKirby5) {
+      function processFields(fields) {
+        for (const fieldName in fields) {
+          const field = fields[fieldName];
+          if (!window[pluginNamespace].functions.isLayoutField(field)) {
+            continue;
+          }
 
+          for (const layout of field) {
+            window[pluginNamespace].functions.addLayoutAttrs(layout);
+          }
+        }
+      }
+
+      const currentContent = panel.content.version('latest');
+      setTimeout(() => {
+        processFields(currentContent);
+      }, 500)
+
+      window.panel.events.on('content.save', ({values}) => {
+        processFields(values);
+      })
+    }
+    else if (isKirby4) {
+      Vue.$store.subscribeAction({
+        after: (action, state) => {
+          if (action.type === "content/create") {
+            // TODO: Refactor to a more elegant solution to wait for the dom to be ready, e.g. MutationObserver on the block IDs
+            setTimeout(() => {
+              for (const fieldName in action.payload.content) {
+                const field = action.payload.content[fieldName];
+                if (!window[pluginNamespace].functions.isLayoutField(field)) {
+                  continue;
+                }
+  
+                for (const layout of field) {
+                  window[pluginNamespace].functions.addLayoutAttrs(layout);
+                }
+              }
+            }, 100);
+          }
+          if (action.type === "content/update") {
+            const field = action.payload?.[1];
+            if (window[pluginNamespace].functions.isLayoutField(field)) {
               for (const layout of field) {
                 window[pluginNamespace].functions.addLayoutAttrs(layout);
               }
             }
-          }, 100);
-        }
-        if (action.type === "content/update") {
-          const field = action.payload?.[1];
-          if (window[pluginNamespace].functions.isLayoutField(field)) {
-            for (const layout of field) {
-              window[pluginNamespace].functions.addLayoutAttrs(layout);
-            }
           }
-        }
-      },
-    });
+        },
+      });
+    }
   },
 });
